20dde6586d9b90f15f5ad3b19e252858911da2bb
t3ssk <te.konecna@icloud.com>
2020-09-04 17:27:57 +0200

    drums done

--- Drum-Kit/index.js
+++ Drum-Kit/index.js
@@ -13,7 +13,49 @@ function handleClick(){
 
 //bubínky
 
-
+var sounds = ["sounds/crash.mp3", "sounds/kick-bass.mp3", "sounds/snare.mp3", "sounds/tom-1.mp3", "sounds/tom-2.mp3", "sounds/tom-3.mp3", "sounds/tom-4.mp3"]
 for (var i=0; i<document.querySelectorAll(".drum").length; i++){
-    document.querySelectorAll("button")[i].addEventListener("click", function(){alert("I got clicked")})
+    document.querySelectorAll("button")[i].addEventListener("click",function(){keySound(this.innerHTML)})
+}
+
+document.addEventListener ("keydown", function(event){
+    keySound(event.key)
+})
+
+function clickSound (audioFile) {
+   
+    { var audio = new Audio(audioFile);
+        audio.play()}
+}
+
+function keySound (key) {
+    
+        switch (key) {
+            case "w":
+                clickSound(sounds[0])
+                break;
+            case "a":
+            clickSound(sounds[1])
+            break;
+            case "s":
+            clickSound(sounds[2])
+            break;
+            case "d":
+            clickSound(sounds[3])
+            break;
+            case "j":
+            clickSound(sounds[4])
+            break;
+            case "k":
+            clickSound(sounds[5])
+            break;
+            case "l":
+            clickSound(sounds[6])
+            break;
+        
+            default:
+                alert("There has been an error")
+                break;
+        
+    }
 }
