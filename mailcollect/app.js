804249e138ce8697c562094f898e9cc67e1ff249
t3ssk <te.konecna@icloud.com>
2020-09-17 16:11:31 +0200

    heroku server

--- mailcollect/app.js
+++ mailcollect/app.js
@@ -50,10 +50,12 @@ app.post('/', function(req, res){
     request.end()
 })
 
+app.post('/failure', function(req, res){
+res.redirect('/')
+})
 
 
-
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log("Server is running");
 })
 
