3c33a9abc2b7a1d7daba430ee62639148ade234e
t3ssk <te.konecna@icloud.com>
2020-09-20 20:21:15 +0200

    pages about and contact

---
+++ blog-template/app.js
@@ -0,0 +1,41 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const homeStartingContent = "Na tomto blogu hodlám sdílet své myšlenky o nově započatém 100 days of code. Není to sice první den, co jsem začala na téhle věci pracovat, ale začnu odznova.  Čístě abych si mohla zaznamenávat a měřit svůj progress. A tohle bude takový můj log. Co mám, v plánu se naučit? Chtěla bych zabrousit do různých sfér fullstack web developlmentu. Ale chtěla bych se hlavně věnovat Reactu a Vue. Potrénuji i templating pro wordpress. A dál se uvidí."
+const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get('/', function(req, res){
+  res.render('home', {article: homeStartingContent })
+})
+
+app.get('/about', function(req,res){
+  res.render('about', {about: aboutContent})
+})
+
+app.get('/contact', function(req,res){
+  res.render('contact', {contact: contactContent})
+})
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
--- calculator/calculator.js
+++ calculator/calculator.js
@@ -6,11 +6,37 @@ app.use(bodyParser.urlencoded({extended:true})) //toto musíme zadat abychom par
 app.get("/", function(req, res){
     res.sendFile(__dirname + '/index.html')
 })
+app.get('/bmicalculator.html', function(req, res){
+    res.sendFile(__dirname + "/bmiCalculator.html")
+})
 app.post('/', function(req, res){
     var num1 = parseInt(req.body.num1, 10)
     var num2 = parseInt(req.body.num2, 10)
     var final = num1 + num2;
     res.send(`Tvůj výsledek je ${final}`)
+})
+
+app.post('/bmicalculator.html', function (req, res){
+    var height = Number(req.body.height)/100;
+    var weight = Number(req.body.weight);
+    var result = (weight/(height*height))
+
+    
+    if(result<18.5){
+        res.send('You are underweight')
+    }
+    else if(result>18.5 && result<24.9){
+        res.send('You are healthy weight')
+    }
+    else if(result>24.9 && result<29.9){
+        res.send('You are overweight')
+    }
+    else if(result > 29.9){
+        res.send('You are obese')
+    }
+    else{ res.send('Something went very wrong')}
+       
+           
 })
 app.listen(3000, function(){
     console.log('running')
